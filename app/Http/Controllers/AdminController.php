8634d663e6cd8bd620cf626def1bece0a9a97a19
Développeur HBP <hbp@macbookppeurhbp.home>
2024-11-06 13:23:11 +0100

    Toutes les methodes du controller AdminController sont prêtes, wip : codage de la logique des methodes.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,7 +2,173 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class AdminController
 {
-    //
+    /**
+     * Dashboard
+     */
+    public function dashboard()
+    {
+        //
+    }
+
+    /**
+     * Utilisateurs
+     */
+    public function getUsers()
+    {
+        // 
+    }
+    
+    public function createUser(Request $request)
+    {
+        if($request->isMethod('post')){
+            // Case formulaire post
+        }
+        // Vue Formulaire
+    }
+
+    public function modifyUser(Request $request, $user_id)
+    {
+        if($request->isMethod('put')){
+            // Case formulaire posté
+        }
+        // Vue formulaire
+    }
+
+    public function deleteUser($user_id)
+    {
+        //
+    }
+
+    /**
+     * Marques
+     */
+    public function getBrands()
+    {
+        //
+    }
+
+    public function createBrand(Request $request)
+    {
+        if($request->isMethod('post')){
+            // Case formulaire posté
+        }
+        // Vue Formulaire
+    }
+
+    public function modifyBrand(Request $request, $brand_id)
+    {
+        if($request->isMethod('put')){
+            // Case formulaire posté
+        }
+        // Vue formulaire
+    }
+
+    public function deleteBrand($brand_id)
+    {
+        //
+    }
+
+    /**
+     * Types
+     */ 
+    public function getTypes()
+    {
+        //
+    }
+
+    public function createType(Request $request)
+    {
+        if($request->isMethod('post')){
+            //
+        }
+        //
+    }
+
+    public function modifyType(Request $request, $type_id)
+    {
+        if($request->isMethod('put')){
+            //
+        }
+        //
+    }
+
+    public function deleteType($type_id)
+    {
+        //
+    }
+
+    /**
+     * Etats / States
+     */
+    public function getStates()
+    {
+        //
+    }
+
+    public function createState(Request $request)
+    {
+        if($request->isMethod('post')){
+            // Case formulaire posté
+        }
+        // Vue formulaire
+    }
+
+    public function modifyState(Request $request, $state_id)
+    {
+        if($request->isMethod('put')){
+            // Case formulaire posté
+        }
+        // Vue formulaire
+    }
+
+    public function deleteState($state_id)
+    {
+        //
+    }
+
+    /**
+     * Produits / Products
+     */
+    public function getProducts()
+    {
+        //
+    }
+
+    public function createProduct(Request $request)
+    {
+        if($request->isMethod('post')){
+            // Case formulaire posté
+        }
+        // Vue formulaire
+    }
+
+    public function modifyProduct(Request $request, $product_id)
+    {
+        if($request->isMethod('put')){
+            // Case formulaire posté
+        }
+        // Vue formulaire
+    }
+
+    public function deleteProduct($product_id)
+    {
+        //
+    }
+
+    /**
+     * Tickets && Stats
+     */
+    public function getTickets()
+    {
+        //
+    }
+
+    public function getStats()
+    {
+        //
+    }
 }
